93286e89bbf84376ec0390132133fa4ac94228a2
Shunpei Kobayashi <skonb@me.com>
2015-07-22 11:45:06 +0900

    Do not query extra pages

--- lib/amazon-review.rb
+++ lib/amazon-review.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require_relative "amazon-review/review"
 
 module AmazonReview
 
@@ -7,35 +8,16 @@ module AmazonReview
     reviews = []
     delay = 0.5
     page = 1
-    
-    # iterate through the pages of reviews
-
-    begin
-      url = "http://www.amazon.co.jp/product-reviews/#{asin}/?ie=UTF8&showViewpoints=0&pageNumber=#{page}&sortBy=bySubmissionDateAscending"
-      html = open(url, "r:CP932").read.encode("UTF-8")
-      doc = Nokogiri::HTML.parse(html)
-      # parse each review
-      new_reviews = 0
-      doc.css("#productReviews td > a[name]").each do |review_html|
-        reviews << Review.new(review_html)
-        new_reviews += 1
-      end       
-      # go to next page
-      page += 1
-      break
-      
-      # delay to prevent 503 errors
-      delay = [0, delay - 0.1].max # decrease delay
-      sleep delay
-      
-    rescue Exception => e # error while parsing (likely a 503)      
-      delay += 0.5 # increase delay
-    
-    end until new_reviews == 0
-    
-    reviews  
+    url = "http://www.amazon.co.jp/product-reviews/#{asin}/?ie=UTF8&showViewpoints=0&pageNumber=#{page}&sortBy=bySubmissionDateAscending"
+    html = open(url, "r:CP932").read.encode("UTF-8")
+    doc = Nokogiri::HTML.parse(html)
+    # parse each review
+    new_reviews = 0
+    doc.css("#productReviews td > a[name]").each do |review_html|
+      reviews << Review.new(review_html)
+      new_reviews += 1
+    end
+    reviews
   end
 
 end
-
-require_relative "amazon-review/review"
